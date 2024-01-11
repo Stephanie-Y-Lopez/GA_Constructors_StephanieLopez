9b66a5880386f46f24923f2f2545e97490e2e94b
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-11 14:20:34 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GA_Constructors_StephanieLopez
+{
+    internal class Program
+    {
+        //Stephanie Lopez 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // A constructor is, 
+            // A constructors role is,
+        }
+    }
+}
